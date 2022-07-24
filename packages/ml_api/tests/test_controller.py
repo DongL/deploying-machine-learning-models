7956c46dfb3b8c31e966ee36a746d922c4f0f22f
Dong Liang <ldifer@gmail.com>
2022-07-23 22:36:03 -0700

    poetry test

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -2,10 +2,10 @@ import os
 import math
 import json
 import io
-from regression_model.regression_model import __version__ as _version
-from regression_model.regression_model.config import config as model_config
-from regression_model.regression_model.processing.data_management import load_dataset
 from api import __version__ as api_version
+from regression_model import __version__ as _version
+from regression_model.config import config as model_config
+from regression_model.processing.data_management import load_dataset
 
 
 # from neural_network_model.config import config as ccn_config
